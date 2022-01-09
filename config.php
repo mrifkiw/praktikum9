72cd632cfa7233366cb39548ff743ba970c63450
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-09 19:32:04 +0700

    feat: add configuration for db

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'praktikum9';
+$databaseUsername = 'root';
+$databasePassword = 'larolaran';
+ 
+$mysqli = mysqli_connect(
+    $databaseHost,
+ $databaseUsername,
+  $databasePassword,
+   $databaseName); 
+?>
