46c3dd8c8d86875454630790f5d1b19095f0232b
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-09 21:01:51 +0700

    feat: add styling

--- index.php
+++ index.php
@@ -11,35 +11,79 @@ $karywawan = query("SELECT * FROM karyawan");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="output.css">
     <title>Praktikum 9</title>
 </head>
 
 <body>
-    <h1>Daftar Karyawan</h1>
 
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Gender</th>
-            <th>Position</th>
-            <th>Status</th>
-            <th>Action</th>
-        </tr>
-        <?php foreach ($karywawan as $item) : ?>
+    <nav class="bg-white border-gray-200 px-6 py-4 shadow">
+        <div class="flex justify-between items-center">
+            <span class="text-lg font-semibold">Karyawanku</span>
+            <ul class="flex gap-4">
+                <li>
+                    <a href="/" class="nav-item target:bg-blue-600" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="/tambah.php" class="nav-item target:bg-blue-600">Tambah Karyawan</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+
+    <h1 class="flex justify-center py-10 font-sans font-medium text-2xl ">Daftar Karyawan</h1>
+
+    <table class="mx-auto rounded-lg shadow">
+        <thead class="bg-gray-100 dark:bg-gray-700">
             <tr>
-                <td><?= $item["id"] ?></td>
-                <td><?= $item["name"] ?></td>
-                <td><?= $item["email"] ?></td>
-                <td><?= $item["address"] ?></td>
-                <td><?= $item["gender"] ?></td>
-                <td><?= $item["position"] ?></td>
-                <td><?= $item["status"] ?></td>
-                <td><a href="hapus.php?id=<?= $item["id"]; ?>" onclick="return confirm('yakin?')">Hapus</a></td>
+                <th class="table-head-cell">
+                    #
+                </th>
+                <th class="table-head-cell">
+                    Name
+                </th>
+                <th class="table-head-cell">
+                    Email
+                </th>
+                <th class="table-head-cell">
+                    Address
+                </th>
+                <th class="table-head-cell ">
+                    Gender
+                </th>
+                <th class="table-head-cell ">
+                    Position
+                </th>
+                <th class="table-head-cell ">
+                    Status
+                </th>
+                <th class="table-head-cell ">
+                    Action
+                </th>
             </tr>
-        <?php endforeach ?>
+        </thead>
+        <tbody>
+            <?php foreach ($karywawan as $item) : ?>
+                <tr class="bg-white border-b">
+                    <td class="table-row-cell"><?= $item["id"] ?></td>
+                    <td class="table-row-cell"><?= $item["name"] ?></td>
+                    <td class="table-row-cell"><?= $item["email"] ?></td>
+                    <td class="table-row-cell"><?= $item["address"] ?></td>
+                    <td class="table-row-cell"><?= $item["gender"] ?></td>
+                    <td class="table-row-cell"><?= $item["position"] ?></td>
+                    <td class="table-row-cell"><?= $item["status"] ?></td>
+                    <td class="table-row-cell">
+                        <a class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 " href="hapus.php?id=<?= $item["id"]; ?>" onclick="return confirm('yakin?')">Hapus</a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+
+
+
 </body>
 
 </html>
