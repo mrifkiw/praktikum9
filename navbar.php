b66f078090796734d289ec19b4610d8856ad65b6
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-09 21:18:09 +0700

    feat: make navbar as component

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<nav class="bg-white border-gray-200 px-6 py-4 shadow">
+        <div class="flex justify-between items-center">
+            <span class="text-lg font-semibold">Karyawanku</span>
+            <ul class="flex gap-4">
+                <li>
+                    <a href="/" class="nav-item target:bg-blue-600" aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="/tambah.php" class="nav-item target:bg-blue-600">Tambah Karyawan</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
